67a80194bd9c5af3bb1bd2c68dd4959c1e2107e2
Jade Allen Cook <jadeallencook@gmail.com>
2016-06-08 23:55:35 -0600

    ⛏ Completed contact & dealer portal

--- contact-form.php
+++ contact-form.php
@@ -1,48 +1,23 @@
 <?php
 
-//Add your information here
-$recipient = "themeturn@gmail.com";
-$successPage = "index.html";
-
-//Don't edit anything below this line
-
-
-//import form information
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
-
-
-$name=stripslashes($name);
-$email=stripslashes($email);
-$subject=stripslashes($subject);
-$message=stripslashes($message);
-$message= "Name: $name, \n\n Message: $message";
-
-/*
-Simple form validation
-check to see if an email and message were entered
-*/
-
-//if no message entered and no email entered print an error
-if (empty($message) && empty($email)){
-print "No email address and no message was entered. <br>Please include an email and a message";
-}
-//if no message entered send print an error
-elseif (empty($message)){
-print "No message was entered.<br>Please include a message.<br>";
-}
-//if no email entered send print an error
-elseif (empty($email)){
-print "No email address was entered.<br>Please include your email. <br>";
-}
-//if the form has both an email and a message
-else {
-
-//mail the form contents
-mail( "$recipient", "$subject", "$message", "From: $email" );
-header("Location: $successPage");
-}
+$admin = 'hello@minnowmike.com';
+$subject = 'Contact From Website';
+$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+$name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+$comments = filter_var($_POST['subject'], FILTER_SANITIZE_STRING) . ' - ' . filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+$emailHTML = 'Name: ' . $name . "\r\n" . "\r\n" . "\r\n" . 'Email: ' . $email . "\r\n" . "\r\n" . 'Comments: ' . $comments;
+mail($admin, "$subject", $emailHTML, "From:" . $email);
 
 ?>
+
+    <center>
+        <h1 style="font-family:Helvetica;font-weight:100;margin-top:25px;">We will be getting ahold of you soon!</h1>
+        <br />
+        <a style="font-family:Helvetica; margin-top:25px; text-decoration: none;" href="index.html">Click here if you are not automatically forwarded.</a>
+    </center>
+
+    <script>
+        setTimeout(function () {
+            window.location.href = "index.html";
+        }, 2000);
+    </script>
